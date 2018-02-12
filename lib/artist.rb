9941de7313e4d2707e0e5615fa57563f9cd08ed3
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 22:38:00 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    self.name = name
+    self.songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def genres
+    self.songs.map { |song| song.genre }
+  end
+end
